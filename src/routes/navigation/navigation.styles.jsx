91a058d60cec5edefb178731c018fa383a1cc110
joeebw <joeegaarciia@gmail.com>
2022-11-02 13:16:06 -0600

    backup

--- src/routes/navigation/navigation.styles.jsx
+++ src/routes/navigation/navigation.styles.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 export const NavigationContainer = styled.div`
   height: 70px;
@@ -7,13 +7,13 @@ export const NavigationContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
-`;
+`
 
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
-  padding: 25px;
-`;
+  padding: 0px 22px ;
+`
 
 export const NavLinks = styled.div`
   width: 50%;
@@ -21,9 +21,11 @@ export const NavLinks = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
-`;
+  font-size: 1.2rem;
+`
 
 export const NavLink = styled(Link)`
-  padding: 10px 15px;
+  padding: 10px 30px;
   cursor: pointer;
-`;
+`
+
